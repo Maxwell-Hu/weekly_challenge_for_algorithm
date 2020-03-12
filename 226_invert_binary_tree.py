e22cbd68e8268fcd1938340c4f9e032c43e4bc68
Zehua Hu <562266259@qq.com>
2020-03-12 13:58:03 +0800

    Add iterative solution for No 226.

--- 226_invert_binary_tree.py
+++ 226_invert_binary_tree.py
@@ -13,3 +13,17 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+class Solution_iterative:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        stack = []
+        if root:
+            stack.append(root)
+        while stack:
+            p = stack.pop()
+            p.left, p.right = p.right, p.left
+            if p.left:
+                stack.append(p.left)
+            if p.right:
+                stack.append(p.right)
+        return root
