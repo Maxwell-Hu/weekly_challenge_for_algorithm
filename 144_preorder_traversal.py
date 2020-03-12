c534277c1b8eff13d5eb5278057658bc05210f96
Zehua Hu <562266259@qq.com>
2020-03-12 14:04:35 +0800

    No. 144

---
+++ 144_preorder_traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        stack = []
+        res = []
+        if root:
+            stack.append(root)
+        while stack:
+            p = stack.pop()
+            res.append(p.val)
+            if p.right:
+                stack.append(p.right)
+            if p.left:
+                stack.append(p.left)
+        return res
