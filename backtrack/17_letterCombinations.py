a8d23307212c1ab10cfb9c09d3e5599087fc9d33
Zehua Hu <562266259@qq.com>
2020-07-29 18:24:53 +0800

    No. 739

--- backtrack/17_letterCombinations.py
+++ backtrack/17_letterCombinations.py
@@ -13,3 +13,20 @@ class Solution:
             return
         for ch in self.mapper[digits[0]]:
             self.incursive(s+ch, digits[1:])
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0: return []
+        mapper = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl',
+                  '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+        res  = []
+
+        def backtrack(s, d):
+            if len(d) == 0:
+                res.append(s)
+                return
+            for ch in mapper[d[0]]:
+                backtrack(s+ch, d[1:])
+
+        backtrack('', digits)
+        return res
