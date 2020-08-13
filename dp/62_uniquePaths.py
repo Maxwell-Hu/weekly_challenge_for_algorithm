a190b1c94266220d5e4ba291c89fd8a5519b3047
Zehua Hu <562266259@qq.com>
2020-08-13 13:24:49 +0800

    No. 62

--- dp/62_uniquePaths.py
+++ dp/62_uniquePaths.py
@@ -9,3 +9,11 @@ class Solution:
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         print(dp)
         return dp[m][n]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * m
+        for i in range(n-1):
+            for j in range(1,m):
+                dp[j] = dp[j-1] + dp[j]
+        return dp[-1]
