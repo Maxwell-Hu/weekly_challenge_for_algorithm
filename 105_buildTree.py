c68082de876f3ddf55ed9f466818ecac267c8fe7
Zehua Hu <562266259@qq.com>
2020-08-17 11:22:44 +0800

    No. 10

--- 105_buildTree.py
+++ 105_buildTree.py
@@ -17,3 +17,14 @@ class Solution:
         node.left = self.buildTree(left_preorder, left_inorder)
         node.right = self.buildTree(right_preorder, right_inorder)
         return node
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if len(preorder) == 0:
+            return None
+        root = TreeNode(preorder[0])
+        index = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:index+1], inorder[:index])
+        root.right = self.buildTree(preorder[index+1:], inorder[index+1:])
+        return root
