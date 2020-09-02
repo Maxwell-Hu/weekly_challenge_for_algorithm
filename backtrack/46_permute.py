4060d3ca7d34e4d72d998a3b9652b1cb54285ffa
Zehua Hu <562266259@qq.com>
2020-09-02 13:57:14 +0800

    01.05

--- backtrack/46_permute.py
+++ backtrack/46_permute.py
@@ -14,3 +14,16 @@ class Solution:
             track.append(num)
             self.backtrack(nums, track)
             track.pop()
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(track, nums_):
+            if len(track) == len(nums):
+                res.append(track)
+            for i, n in enumerate(nums_):
+                track.append(n)
+                backtrack(track[:], nums_[:i]+nums_[i+1:])
+                track.pop()
+        res = []
+        backtrack([], nums)
+        return res
