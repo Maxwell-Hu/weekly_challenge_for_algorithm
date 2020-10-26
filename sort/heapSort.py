4b9e1f90d04560b4bf79dec0dd2503bff6c771a5
Zehua Hu <t-zehhu@microsoft.com>
2020-10-26 16:09:29 +0800

    No. 162 215

--- sort/heapSort.py
+++ sort/heapSort.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-def heapSort(lst):
-	def sift_down(start, end):
-		root = start
-		while True:
-			child = 2 * root + 1
-			if child > end:
-				break
-			if child + 1 <= end and lst[child] < lst[child + 1]:
-				child += 1
-			if lst[root] < lst[child]:
-				lst[root], lst[child] = lst[child], lst[root]
-				root = child
-			else:
-				break
 
-	# Initialize heap.
-	for start in range((len(lst)-2), -1, -1):
-		sift_down(start, len(lst)-1)
+def heapify(arr, n, i):
+	l = i*2 + 1
+	r = i*2 + 2
+	largest = i
 
-	# Heap sort.
-	for end in range(len(lst)-1, 0, -1):
-		lst[0], lst[end] = lst[end], lst[0]
-		sift_down(0, end - 1)
-	return lst
+	if l < n and arr[l] > arr[largest]:
+		largest = l
 
+	if r < n and arr[r] > arr[largest]:
+		largest = r
+	
+	if largest != i:
+		arr[largest], arr[i] = arr[i], arr[largest]
+		heapify(arr, n, largest)
+
+def heapSort(arr):
+	n = len(arr)
+
+	for i in range(n, -1, -1):
+		heapify(arr, n, i)
+
+	for i in range(n-1, 0, -1):
+		arr[0], arr[i] = arr[i], arr[0]
+		heapify(arr, i, 0)
 
 if __name__ == "__main__":
-    l = [9, 2, 1, 7, 6, 8, 5, 3, 4]
-    heap_sort(l)
+	# l = [9, 2, 1, 7, 6, 8, 5, 3, 4]
+	l = [2,1]
+	heapSort(l)
+	print(l)
