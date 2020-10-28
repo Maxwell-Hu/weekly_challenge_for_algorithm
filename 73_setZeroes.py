c93403c96973042a9be2273191c17d901e952182
Zehua Hu <t-zehhu@microsoft.com>
2020-10-28 14:21:32 +0800

    No. 3

--- 73_setZeroes.py
+++ 73_setZeroes.py
@@ -27,3 +27,20 @@ class Solution:
                 j += 1
         setZero(row, column)
         return matrix
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        column, row = set(), set()
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    row.add(i)
+                    column.add(j)
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in row or j in column:
+                    matrix[i][j] = 0
