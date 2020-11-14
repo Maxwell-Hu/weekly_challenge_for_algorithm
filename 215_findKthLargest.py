fef0f62dc2f88e6b4da245be03c9b06c1be41c26
Zehua Hu <562266259@qq.com>
2020-11-14 13:28:31 +0800

    No. 215

--- 215_findKthLargest.py
+++ 215_findKthLargest.py
@@ -27,3 +27,33 @@ class Solution:
         if largest != i:
             nums[largest], nums[i] = nums[i], nums[largest]
             self.heapify(nums, n, largest)
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        import random
+        if not nums: return None
+
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            index = self.__partition(nums, left, right)
+            if len(nums)-index == k:
+                return nums[index]
+            elif len(nums)-index < k:
+                right = index - 1
+            else:
+                left = index + 1
+
+    def __partition(self, nums, left, right):
+        ri = random.randint(left, right)
+        nums[left], nums[ri] = nums[ri], nums[left]
+        pivot = nums[left]
+        j = left
+
+        for i in range(left+1, right+1):
+            if nums[i] < pivot:
+                j += 1
+                nums[j], nums[i] = nums[i], nums[j]
+
+        nums[j], nums[left] = nums[left], nums[j]
+        return j
